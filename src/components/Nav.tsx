f71e8d12a0ca3c68639d071ecc39a1889f105dc4
cl1996 <1430125208@qq.com>
2020-05-19 12:13:54 +0800

    icon part

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,34 +1,53 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
-import x from 'icons/money.svg'
+
+require('icons/money.svg');
+require('icons/label.svg');
+require('icons/statistics.svg');
+
 
 const NavWrapper = styled.nav`
       box-shadow: 0 0 3px rgba(0,0,0,0.3);
       > ul{
       display: flex;
       > li{
+      display: flex;
       width: 33.3333333%;
       text-align: center;
-      padding: 16px;
+      align-items: center;
+      flex-direction: column;
+        > .icon{
+        width: 24px;
+        height: 24px;
+             }
          }
       }
 `;
-const Nav = ()=>{
+const Nav = () => {
   return (
     <NavWrapper>
       <ul>
         <li>
-          <Link to="/tags">标签</Link>
-        </li>
-        <li>
+          <svg className="icon">
+            <use xlinkHref="#money"/>
+          </svg>
           <Link to="/money">记账</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#label"/>
+          </svg>
+          <Link to="/tags">标签</Link>
+        </li>
+        <li>
+          <svg className="icon">
+            <use xlinkHref="#statistics"/>
+          </svg>
           <Link to="/statistics">统计</Link>
         </li>
       </ul>
     </NavWrapper>
-  )
-}
-export {Nav}
+  );
+};
+export {Nav};
