33c18785bab99ac45465c778747c1ab9d131c363
cl1996 <1430125208@qq.com>
2020-05-22 15:31:50 +0800

    收集表单数据

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,5 +1,5 @@
 import {Layout} from 'components/Layout';
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import {NoteSection} from './Money/NoteSection';
 import {NumberPadSection} from './Money/NumberPadSection';
@@ -11,14 +11,26 @@ const MyLayout = styled(Layout)`
     flex-direction: column;
 `;
 
+
+type Type = '-' | '+'
+
 function Money() {
+  const [selected, setSelected] = useState(
+    {
+      tags: [] as string[],
+      note: '',
+      type: '-' as Type,
+      amount: 0
+    }
+  );
   return (
     <MyLayout>
-      <TagsSection/>
-      <NoteSection/>
-      <TypeSection/>
-      <NumberPadSection/>
-
+      <TagsSection value={selected.tags} onChange={(tags) => setSelected({...selected, tags: tags})}/>
+      <NoteSection value={selected.note} onChange={(note) => setSelected({...selected, note: note})}/>
+      <TypeSection value={selected.type} onChange={(type) => setSelected({...selected, type: type})}/>
+      <NumberPadSection value={selected.amount}
+                        onOk={()=>{}}
+                        onChange={(amount) => setSelected({...selected, amount:amount})}/>
     </MyLayout>
   );
 }
--- src/views/Money/TypeSection.tsx
+++ src/views/Money/TypeSection.tsx
@@ -24,20 +24,23 @@ const Wrapper = styled.section`
        }
     }
 `;
-const TypeSection: React.FC = () => {
-  const typeMap = {'-':'支出','+':'收入'}
+type Props = {
+  value: '-' | '+',
+  onChange: (value:'-' | '+') => void
+}
+const TypeSection: React.FC<Props> = (props) => {
+  const typeMap = {'-': '支出', '+': '收入'};
   type Keys = keyof typeof typeMap
-  const [typeList] = useState<Keys[]>(['+','-'])
-  const [type, setType]= useState('-')
-
+  const [typeList] = useState<Keys[]>(['+', '-']);
+  const type  =  props.value
   return (
     <Wrapper>
       <ul>
-        {typeList.map(c=>
-          <li className={type===c?'selected':''}
-              onClick={()=>{setType(c)}}
+        {typeList.map(c =>
+          <li className={type === c ? 'selected' : ''}
+              onClick={() => {props.onChange(c);}}
               key={c}
-        >{typeMap[c]}</li>)}
+          >{typeMap[c]}</li>)}
       </ul>
     </Wrapper>
   );
