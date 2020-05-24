b36acdeb68372af3292e517156a89ec0ba06e25d
cl1996 <1430125208@qq.com>
2020-05-24 16:04:46 +0800

    创建Input组件

--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -1,50 +1,26 @@
 import styled from 'styled-components';
-import React, {useRef} from 'react';
+import React, {ChangeEventHandler, useRef} from 'react';
+import {Input} from '../../components/Input';
 
 const Wrapper = styled.section`
     background-color: #f5f5f5;
     font-size: 14px;
     padding:12px 16px ;
-    >label {
-    display: flex;
-    align-items: center;
-        > span{
-        margin-right: 16px;
-        white-space: nowrap;
-        }
-        > input {
-        display: block;
-        width: 100%;
-        height: 72px;
-        background: none ;
-        border: none;
-        }
-    }
  `;
 
-type Props ={
-  value:string,
-  onChange: (value:string)=>void
+type Props = {
+  value: string,
+  onChange: (value: string) => void
 }
 const NoteSection: React.FC<Props> = (props) => {
-  const note = props.value
-  const refInput = useRef<HTMLInputElement>(null)
-  const X = ()=>{
-    if (refInput.current !== null){
-    props.onChange(refInput.current.value)
-      console.log(refInput.current.value);
-    }
-  }
+  const note = props.value;
+  const onChange:ChangeEventHandler<HTMLInputElement>= (e) => {
+      props.onChange(e.target.value);
+  };
   return (
     <Wrapper>
-      <label>
-        <span> 备注</span>
-        <input type="text" placeholder="请在这里写备注噢~"
-               ref={refInput}
-               defaultValue={note}
-               onBlur={X}
-          />
-      </label>
+      <Input label="备注" type="text" value={note} onChange={onChange}>
+      </Input>
     </Wrapper>
   );
 };
